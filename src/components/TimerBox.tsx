79ba3067e829556fa85fdd03077439db973a7dcc
Oscar Liu <44384935+IAmOscar-Liu@users.noreply.github.com>
2023-04-28 12:24:25 +0800

    Update TimerBox.tsx

--- src/components/TimerBox.tsx
+++ src/components/TimerBox.tsx
@@ -1,7 +1,7 @@
 import { useTimerStore } from "../timerStore";
 
 function TimerBox() {
-  const time = useTimerStore((data) => data.time);
+  const time = useTimerStore((state) => state.time);
 
   return <p className="timer-box">{(time / 1000).toFixed(2)}</p>;
 }
